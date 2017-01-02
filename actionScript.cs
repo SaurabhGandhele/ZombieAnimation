84aeaa704304ae3bd75e622b43791bba380e3505
Saurabh Gandhele <saurabhgandhele@gmail.com>
2017-01-02 02:50:25 -0500

    Add files via upload

---
+++ actionScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+using Vuforia;
+
+
+public class actionScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+
+	// Use this for initialization
+	
+private GameObject vbButtonObject;
+private GameObject zombie;
+void Start () 
+{
+
+	vbButtonObject = GameObject.Find("actionButton");
+	zombie = GameObject.Find("zombie");
+	vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+}
+	
+	
+
+public void OnButtonPressed (VirtualButtonAbstractBehaviour vb)
+{
+	zombie.GetComponent<Animation>().Play();
+}
+
+public void OnButtonReleased (VirtualButtonAbstractBehaviour vb)
+{
+	zombie.GetComponent<Animation>().Stop();
+}
+
+}
